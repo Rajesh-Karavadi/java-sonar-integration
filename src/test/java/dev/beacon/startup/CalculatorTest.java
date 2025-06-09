ecb2430973b7bb84cee3c17df2c064e18ab1e851
Rajesh Karavadi <rajesh.karavadi@gmail.com>
2025-06-09 18:52:58 +0530

    FixedTestExecution

--- src/test/java/dev/beacon/startup/CalculatorTest.java
+++ src/test/java/dev/beacon/startup/CalculatorTest.java
@@ -1,6 +1,6 @@
 package dev.beacon.startup;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
--- src/test/java/dev/beacon/startup/SampleTest.java
+++
@@ -1,8 +0,0 @@
-package dev.beacon.startup;
-
-class SampleTest {
-
-    public static void main(String[] args) {
-        System.out.println("Hello!! This is my sampple Java app.");
-    }
-}
